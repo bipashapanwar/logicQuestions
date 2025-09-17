06f64ea0aaa6f446e7c4d51dedfbe34069681800
Bipasha Panwar <bipashapanwar10@gmail.com>
2025-09-18 01:02:22 +0530

    Time: 0 ms (100%), Space: 40.7 MB (49.69%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sum(int num1, int num2) {
+       return (num1+num2); 
+    }
+    public static void main(String args[]){
+        Solution s=new Solution();
+        System.out.println(s.sum(12,5));
+        System.out.println(s.sum(-10,4));
+    }
+}
