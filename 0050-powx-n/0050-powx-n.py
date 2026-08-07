92c2f682008754b557fa09dcad5a9b5e87b8e906
Bipasha Panwar <bipashapanwar10@gmail.com>
2026-08-07 19:13:17 +0530

    Time: 0 ms (100%), Space: 12.5 MB (27.92%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n==0:
+            return 1
+        if n<0:
+            x=1/x
+            n=-n
+        half=self.myPow(x,n//2)
+        if n%2==0:
+            return half*half
+        else:
+            return x*half*half
+        
+        
+        
